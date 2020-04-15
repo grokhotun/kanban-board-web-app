97fd3572645d4bafbf721e44bdda60c9ca8c9266
Askar_Yalyaev <askarrussia@gmail.com>
2020-04-15 14:03:41 +0500

    Updates

---
+++ precache-manifest.68470833c2a13e8b7f4850fd3362a90f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4284a199864f2cb58ff7bcfef7e0a4b",
+    "url": "/kanban-board-web-app/index.html"
+  },
+  {
+    "revision": "e762ea2561cfa6d01f07",
+    "url": "/kanban-board-web-app/static/css/main.fe8a8124.chunk.css"
+  },
+  {
+    "revision": "64400305382367d27a02",
+    "url": "/kanban-board-web-app/static/js/2.e53e7875.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kanban-board-web-app/static/js/2.e53e7875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e762ea2561cfa6d01f07",
+    "url": "/kanban-board-web-app/static/js/main.35d019b5.chunk.js"
+  },
+  {
+    "revision": "cecae00e5e9363439981",
+    "url": "/kanban-board-web-app/static/js/runtime-main.9e843a7a.js"
+  },
+  {
+    "revision": "b4e7124aebe83563440fc77620364520",
+    "url": "/kanban-board-web-app/static/media/add.b4e7124a.svg"
+  },
+  {
+    "revision": "e82c0deb46d38f804bbe1180dc68b2a3",
+    "url": "/kanban-board-web-app/static/media/clear.e82c0deb.svg"
+  },
+  {
+    "revision": "c91a89fa934b17623619039a318c3ee4",
+    "url": "/kanban-board-web-app/static/media/remove.c91a89fa.svg"
+  }
+]);
